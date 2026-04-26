cd63b027dae45caef1e9c71edc8b2408ab31707b
PUSHCLASH <kaushtubhanime09@gmail.com>
2026-04-26 19:30:09 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -317,4 +317,6 @@ with app.app_context():
     init_db()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    # Get the port from Render’s environment variable, or use 5000 locally
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
